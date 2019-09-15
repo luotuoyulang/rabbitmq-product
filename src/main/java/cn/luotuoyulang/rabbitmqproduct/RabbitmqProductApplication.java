8503395c99f7b18ce2452da81f5d3ec4174c4b4c
km611112 <sunyukun>
2019-09-15 23:44:16 +0800

    提交   rabbitmq 生产者

---
+++ src/main/java/cn/luotuoyulang/rabbitmqproduct/RabbitmqProductApplication.java
@@ -0,0 +1,13 @@
+package cn.luotuoyulang.rabbitmqproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqProductApplication.class, args);
+    }
+
+}
